88e7ab97ca5ca993a2be660300016a6cc4731c28
Nicolas Udy <dev@udy.io>
2017-08-01 02:28:37 -0400

    Working use command

--- src/use/install.js
+++ src/use/install.js
@@ -10,6 +10,7 @@ export default function() {
   this.spinner.succeed().start(' Load template')
   this.template = require(path.resolve(root, 'template.json'))
 
+  // assert that specified file exists in target
   if (!this.template.checkfile) return Promise.resolve()
   return new Promise((resolve, reject) => {
     fs.access(this.template.checkfile, fs.constants.R_OK | fs.constants.W_OK, error => {
@@ -17,37 +18,40 @@ export default function() {
       resolve()
     })
   }).then(() => {
+    // remove files
     this.spinner.succeed().start(' Remove unnecessary files')
 
     if (!this.template.remove || this.options.safe) return Promise.resolve()
     return del(this.template.remove)
   }).then(() => {
+    // copy template directories and files
     let { ignore = [] } = this.template
-    let filter = ['**/*', '!.git/**', '!template.json', ...ignore.map(f => '!' + f)]
-    console.log(filter)
     this.spinner.succeed().start(' Copy template files')
 
     return copy(root, process.cwd(), {
-      filter,
       dot: true,
-      overwrite: !this.options.safe
+      overwrite: !this.options.safe,
+      filter: ['**/*', '!.git/**', '!template.json', ...ignore.map(f => '!' + f)]
     })
   }).then(() => {
+    // edit target package.json
     let edit = this.template.edit
-
     this.spinner.succeed().start(' Edit package')
 
     if (!edit) return Promise.resolve()
     return new Promise((resolve, reject) => {
+      // load target package.json
       fs.readFile('./package.json', 'utf8', (error, pkg) => {
         if (error) return reject(error)
         try { pkg = JSON.parse(pkg) }
         catch (error) { return reject(error) }
 
+        // apply each edit by type and mutation
         Object.keys(edit).forEach(key => {
           pkg[key] = parseKey(pkg[key], edit[key])
         })
 
+        // save changes
         fs.writeFile('./package.json', JSON.stringify(pkg, null, 2), 'utf8', error => {
           if (error) return reject(error)
           resolve()
