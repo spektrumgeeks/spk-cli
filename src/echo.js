ac96b20fbf0c86274fe47a1ca9827fa230bd71e1
Nicolas Udy <dev@udy.io>
2017-08-07 16:06:26 -0400

    Add echo

---
+++ src/echo.js
@@ -0,0 +1,19 @@
+import chalk from 'chalk'
+import wrap from 'wrap-ainsi'
+import symbols from 'log-symbols'
+
+export default ({ msg, status, whitespace, cols = 60 }) => {
+  let i = 0
+
+  if (!whitespace) msg = msg.replace('\n', ' ').replace('\t', ' ')
+  msg = msg.replace('<br%>', '\n').replace('<tb%>', '\t')
+
+  while (/<%([^>]+)>([^<]+)<%>/.test(msg) && ++i < 5) {
+    msg = msg.replace(/<%([^>]+)>([^<]+)<%>/g, (m, $1, $2) => (chalk[$1]) ? chalk[$1]($2) : $2)
+  }
+
+  if (status && symbols[status]) msg = symbols[status] + ' ' + msg
+  if (parseInt(cols)) msg = wrap(msg, cols)
+
+  console.log(msg)
+}
--- src/postinstall.js
+++ src/postinstall.js
@@ -1,5 +1,6 @@
 import path from 'path'
 import fs from 'fs-extra'
+import echo from './echo'
 import chalk from 'chalk'
 import ascii from './ascii'
 import git from 'simple-git'
@@ -24,22 +25,27 @@ const storeSetup = () => Object.keys(storeBase).map(key => {
 })
 
 // deliver info about issues with git over ssh
-console.log(ascii, '\n\n', symbol.info, 'spk-cli uses git to keep templates up to date but cannot',
-  'authenticate connections over ssh if your key uses a passphrase. In such cases you will need to',
-  'provide a token to connect over https.\n\n',
-  chalk.bold('To complete installation, access to BitBucket will be required.'))
+echo({
+  status: 'info',
+  msg: `<br%><br%>${ascii}<br%><br%>spk-cli uses git to keep templates up to date but can't
+    authenticate connections over ssh if your key uses a passphrase. In such cases you will need to
+    provide a token to connect over https.<br%><%underline><%yellow>To complete installation, access
+    to BitBucket will be required.<%><%><br%>`
+})
 
 inquire.prompt([
   { type: 'confirm', name: 'passphrase', default: false, message: 'Do you need to set a token?' }
 ])
-
 // get bitbucket token if need be
 .then(({ passphrase }) => {
   if (!passphrase) return Promise.resolve()
 
-  console.log('To generate your BitBucket app password, go to "BitBucket settings" for your',
-    'account and generate a token under "Access management > App passwords".\n\n',
-    chalk.bold('Minimum required permissions are "Repositories > Read".'))
+  echo({
+    status: 'info',
+    msg: `To generate your BitBucket app password, go to <%underline><%blue>BitBucket settings<%><%>
+      in your account. Under <%underline><%blue>Access management > App passwords<%><%>, generate
+      your token with <%underline>Repositories.Read<%> permissions.<br%>`
+  })
 
   return inquire.prompt([
     { type: 'input', name: 'user', message: 'BitBucket username:' },
@@ -52,21 +58,23 @@ inquire.prompt([
     return Promise.resolve()
   })
 })
-
 // write store to disk and ensure templates is empty
 .then(() => Promise.all([
   ...storeSetup(),
   fs.emptyDir(path.resolve(store, 'templates'))
 ]))
-
 // clone master repo
 .then(() => new Promise((resolve, reject) => {
   git(store).clone(resolveRepo(config.master), 'templates', error => {
-    if (error) return reject('Could not clone templates repo\n\n' + error)
+    if (error) return reject('Could not clone templates repo:\n' + error)
     process.exit(0)
   })
 }))
 .catch(err => {
-  console.log(symbols.error, 'An error occured during installation\n\n', err)
+  echo({
+    status: 'error',
+    whitespace: true,
+    msg: `<%red>An error occured during installation<%>\n\n${err}`
+  })
   process.exit(1)
 })
